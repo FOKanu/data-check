7e2eec1742fcd29fed6d3c6f9d93570e6aa967a9
Francis Okorie Iyeke Kanu <francisokore15@gmail.com>
2025-04-14 16:31:34 +0200

    solved for the area of  circle

--- demo.py
+++ demo.py
@@ -1,7 +1,22 @@
 """Dummy challenge for Kitt Demo"""
+# defining Pie
+import math
+pi = math.pi
 
 
 
 def circle_area(radius):
+
+    #Scenario 1: radius is negative
+    if radius < 0:
+        return 0
+    else:
+        circle_area = pi * (radius ** 2)
+        return circle_area
+
+
+
+
+
     """Returns the area of the circle of given radius"""
     pass  # YOUR CODE HERE
